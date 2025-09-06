c036d67f6fb10688b70aa40d0a9fc0791403e5dd
platypus-perry03 <choe31904@gmail.com>
2025-09-06 10:59:56 +0900

    feat: Expand switch statement in 2_switch_keyword.dart

--- lib/week01/1.6/2_switch_keyword.dart
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -9,9 +9,22 @@ void main() {
 
   switch (status) {
     case Status.approved:
-    // approved값이기 때문에 다음 코드가 실행됩니다.
-    print('승인 상태입니다.');
-    break;
-    
+      // approved값이기 때문에 다음 코드가 실행됩니다.
+      print('승인 상태입니다.');
+      break;
+    case Status.pending:
+      print('대기 상태입니다.');
+      break;
+    case Status.rejected:
+      print('거절 상태입니다.');
+      break;
+    default:
+      print('알 수 없는 상태입니다.');
   }
+
+
+  // Enum의 모든 수를
+  // 리스트로 반환합니다.
+
+  print(Status.values);
 }
