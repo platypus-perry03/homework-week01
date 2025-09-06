1bfaf9fee1297fec6ff7a362c1bed9de716df596
platypus-perry03 <choe31904@gmail.com>
2025-09-06 10:58:26 +0900

    feat(week01): Implement switch statement example using enum

---
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -0,0 +1,17 @@
+enum Status {
+  approved,
+  pending,
+  rejected,
+}
+
+void main() {
+  Status status = Status.approved;
+
+  switch (status) {
+    case Status.approved:
+    // approved값이기 때문에 다음 코드가 실행됩니다.
+    print('승인 상태입니다.');
+    break;
+    
+  }
+}
