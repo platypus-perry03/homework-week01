165d809c3ed0dab6bfa3212d41191874cb233fe5
platypus-perry03 <choe31904@gmail.com>
2025-09-06 10:32:56 +0900

    feat: Implement Set type example in 1_set_type.dart

---
+++ lib/week01/1.4.3/1_set_type.dart
@@ -0,0 +1,10 @@
+void main() {
+  Set<String> blackPink = {'로제', '지수', '리사', '제니', '제니'}; // 제니 중복
+
+  print(blackPink);
+  print(blackPink.contains('로제'));  // 값이 있는지 확인하기
+  print(blackPink.toList());          // 리스트로 변환하기
+
+  List<String> blackPink2 = ['로제', '지수', '지수'];
+  print(Set.from(blackPink2)); // List 타입을 Set 타입으로 변환
+}
