e88ab565b44cb68513d453fd29e897440971546e
platypus-perry03 <choe31904@gmail.com>
2025-09-14 09:24:40 +0900

    feat: Add tuple example with type annotation

---
+++ lib/week02/4.1/1.dart
@@ -0,0 +1,7 @@
+void main() {
+  //정확한 위치에 어떤 타입의 값이 입력될지 지정할 수 있습니다.
+  // (String, int)는 첫 번째 값은 String 타입이고 두 번째 값은 int 타입입니다.
+  (String, int) minji = ('민지', 20);
+  // ('민지', 20) 출력
+  print(minji);
+}
