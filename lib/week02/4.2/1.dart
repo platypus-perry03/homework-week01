c335884a53ce698cedccd1353eab587dd31944a5
platypus-perry03 <choe31904@gmail.com>
2025-09-14 09:31:40 +0900

    feat: Introduce list destructuring example in Dart

---
+++ lib/week02/4.2/1.dart
@@ -0,0 +1,12 @@
+void main() {
+  // 아래 코드와 같지만 구조 분해를 사용하면 한 줄에 해결할 수 있습니다.
+  // final newJeans = ['민지', '해린'];
+  // final minji = newJeans[0];
+  // final haerin = newJeans[1];
+  final [minji, haerin] = ['민지', '해린'];
+
+  // 민지 출력
+  print(minji);
+  // 해린 출력
+  print(haerin);
+}
