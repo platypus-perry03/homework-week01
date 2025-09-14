2ff3751f02ab3fb7fb86e67bfaf5d1a5309939c3
platypus-perry03 <choe31904@gmail.com>
2025-09-14 09:39:40 +0900

    feat(week02/4.3): Implement switch expression for Korean to English day conversion

--- lib/week02/4.3/1.dart
+++ lib/week02/4.3/1.dart
@@ -1,3 +1,20 @@
 void main() {
-  
+  String dayKor = '월요일';
+
+  // switch문이 함수처럼 값을 변환합니다.
+  String dayEnglish = switch (dayKor) {
+    // '=>'를 사용하면 switch문 조건에 맞을 때 값을 변환할 수 있습니다.
+    '월요일' => 'Monday',
+    '화요일' => 'Tuesday',
+    '수요일' => 'Wednesday',
+    '목요일' => 'Thursday',
+    '금요일' => 'Friday',
+    '토요일' => 'Saturday',
+    '일요일' => 'Sunday',
+    // _는 default와 같은 의미로 사용됩니다.
+    _ => 'Not Found',
+  };
+
+  // Monday 출력
+  print(dayEnglish);
 }
