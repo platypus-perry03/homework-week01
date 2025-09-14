ceb5d75f8f90e7e2f48f57427e7b2941a4dd7d4f
platypus-perry03 <choe31904@gmail.com>
2025-09-14 09:43:40 +0900

    feat: Implement pattern matching in switcher function

--- lib/week02/4.3/2.dart
+++ lib/week02/4.3/2.dart
@@ -10,6 +10,18 @@ void switcher(dynamic anything) {
       break;
     // 3개의 값이 들어 있는 리스트를 모두 매치합니다.
     case [_, _, _]:
-      
+      print('match [_,_,_]');
+      break;
+    // 첫 번째와 두 번째 값에 int가 입력된 리스트를 매치합니다.
+    case [int a, int b]:
+      print('match: [int $a, int $b]');
+      break;
+    // 첫 번째 값에 String, 두 번째 값에 int가 입력된 Record 타입을 매치합니다.
+    case (String a, int b):
+      print('match: (String: $a, int: $b)');
+      break;
+    // 아무것도 매치되지 않을 경우 실행합니다.
+    default:
+      print('no match');
   }
 }
